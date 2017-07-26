9694fe70239b1a2e0f9465405f394d58a3ec8c63
zhangy <15249088687@163.com>
2017-07-26 16:29:04 +0800

    qw

---
+++ api/src/main/java/com/kczd/api/net/HttpEngine.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (C) 2015. Keegan小钢（http://keeganlee.me）
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.kczd.api.net;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Map;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * Http引擎处理类
+ *
+ * @author Keegan小钢
+ * @date 15/6/21
+ * @version 1.0
+ */
+public class HttpEngine {
+    private final static String TAG = "HttpEngine";
+    private final static String SERVER_URL = "login/login";
+    private static final String BASE_URL = "http://www.weiantang.com/index.php/api/login/login";//请求接口根地址
+    private static HttpEngine instance = null;
+
+    private HttpEngine() {
+
+    }
+
+    public static HttpEngine getInstance() {
+        if (instance == null) {
+            instance = new HttpEngine();
+        }
+        return instance;
+    }
+
+    public <T> T postHandle(Map<String, String> paramsMap, final Type typeOfT) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody=new FormBody.Builder()
+                .add("mobile","17629047878")
+                .add("password","123456")
+                .build();
+        Request request=new Request.Builder()
+                .url(BASE_URL)
+                .post(requestBody)
+                .build();
+        try {
+            Response response=client.newCall(request).execute();
+            String strResult=response.body().string();
+            Gson gson = new Gson();
+            return gson.fromJson(strResult,typeOfT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+          return null;
+    }
+}
---
+++ app/src/main/java/com/kczd/design_android/LoginActivity.java
@@ -0,0 +1,60 @@
+package com.kczd.design_android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.kczd.core.ActionCallbackListener;
+
+public class LoginActivity extends KBaseActivity {
+
+    private EditText phoneEdit;
+    private EditText passwordEdit;
+    private Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // 初始化View
+        initViews();
+    }
+    // 初始化View
+    private void initViews() {
+        phoneEdit = (EditText) findViewById(R.id.edit_phone);
+        passwordEdit = (EditText) findViewById(R.id.edit_password);
+        loginBtn = (Button) findViewById(R.id.btn_login);
+    }
+
+    // 准备登录
+    public void toLogin(View view) {
+        String loginName = phoneEdit.getText().toString();
+        String password = passwordEdit.getText().toString();
+        loginBtn.setEnabled(false);
+        this.appAction.login(loginName, password, new ActionCallbackListener<Void>() {
+            @Override
+            public void onSuccess(Void data) {
+                Toast.makeText(context, R.string.toast_login_success, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onFailure(String errorEvent, String message) {
+                Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+                loginBtn.setEnabled(true);
+            }
+        });
+    }
+
+    // 进入注册页
+    public void toRegister(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
